8c70285188c207ddb41d3efed2b63ec18210d160
hussamkhatib <hussamkhatib@gmail.com>
2022-10-19 12:25:09 +0530

    fix build errors

--- src/components/NavBar/Navbar.tsx
+++ src/components/NavBar/Navbar.tsx
@@ -1,7 +1,18 @@
+import { FC } from "react";
 import CommandPalette from "./CommandPallete";
 import SelectMapType from "./SelectMapType";
 
-const Navbar = ({ onSelectCity }) => {
+type Props = {
+  onSelectCity: ({
+    longitude,
+    latitude,
+  }: {
+    longitude: number;
+    latitude: number;
+  }) => void;
+};
+
+const Navbar: FC<Props> = ({ onSelectCity }) => {
   return (
     <nav className="fixed w-full top-0 z-30 bg-white flex h-10 justify-between items-center px-2 md:px-4">
       <h1 className="text-xl font-semibold">Kyupid</h1>
